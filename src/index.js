3dd4cf9979758d0ae3a0ebff0076a74264f09b51
malekylik <malekylik@gmail.com>
2018-03-01 23:35:16 +0300

    master/feature impliment lockedCandidate method

--- src/index.js
+++ src/index.js
@@ -57,6 +57,7 @@ function solveSudoku1(matrix) {
     }
   }
   var debug;
+  
 
 
 
@@ -223,7 +224,6 @@ function solveSudoku1(matrix) {
       for(let j = (cof % 3 * 3) ; j < 3 * (cof % 3 + 1); j++){
         for(let k = 0; k < length; k++){
           if(field[i][j][k]){
-            // console.dir("i " + i + " j " + j);
             square[cof][k]++;            
           }
         }
@@ -254,14 +254,145 @@ function solveSudoku1(matrix) {
   }
 
 
+  var debX = [];
+  var debY = [];
+
+  function lockedCandidate(field){
+    let square = {};  
+    for(let cof = 0; cof < 9; cof++){
+      for(let i = Math.floor(cof / 3) * 3; i < (Math.floor(cof / 3) + 1) * 3; i++){
+        for(let j = (cof % 3 * 3) ; j < 3 * (cof % 3 + 1); j++){
+          for(let k = 0; k < length; k++){
+            if(field[i][j][k]){
+              if(square[k] == undefined){
+                square[k] = {};
+                square[k][cof] = {
+                  value:1
+                };
+             square[k][cof]['indices'] = [];
+              square[k][cof]['indices'].push({
+              i:i,
+              j:j
+            });
+              }else{
+                if(square[k][cof] == undefined){
+                  square[k][cof] = {
+                    value:1
+                  };
+               square[k][cof]['indices'] = [];
+                square[k][cof]['indices'].push({
+                i:i,
+                j:j
+              });
+                }else{
+                  square[k][cof].value++;
+                  square[k][cof]['indices'].push({
+                    i:i,
+                    j:j
+                  });
+                }
+              }
+        
+            }
+          }
+        }
+        }      
+      }
+
+      for(let i in square){
+        for(let j in square[i]){
+          if(square[i][j].value < 2 || square[i][j].value > 3){
+            delete square[i][j];
+          }
+        }
+        if(Object.keys(square[i]).length == 0) delete square[i];
+      }
+
+      for(let i in square){
+        for(let j in square[i]){
+          let iCount = 0;
+          let jCount = 0;
+          for(let k = 0; k < square[i][j]['indices'].length - 1 ; k++){
+            if(square[i][j]['indices'][k].i == square[i][j]['indices'][k + 1].i) iCount++;
+            if(square[i][j]['indices'][k].j == square[i][j]['indices'][k + 1].j) jCount++;
+        }
+
+        if(iCount != square[i][j]['indices'].length - 1
+         && jCount != square[i][j]['indices'].length - 1) delete square[i][j];
+        else if(iCount == square[i][j]['indices'].length - 1) square[i][j]['isRow'] = true;
+        else square[i][j]['isRow'] = false;                  
+        if(Object.keys(square[i]).length == 0) delete square[i];
+      }
+    }
+
+
+       debX = [];
+       debY = [];
+      for(let i in square){
+        for(let j in square[i]){
+          if(square[i][j]['isRow']){
+            for(let k = 0; k < length; k++ ){
+              let condition = square[i][j]['indices'][0].j != k && square[i][j]['indices'][1].j != k;
+              if(square[i][j]['indices'][2] != undefined){
+                condition = condition && (square[i][j]['indices'][2].j != k);
+              }
+             
+              if(condition){
+                field[square[i][j]['indices'][0].i][k][i] = false;
+                debX.push({
+                  i:square[i][j]['indices'][0].i,
+                  j:k,
+                  value:i
+                });
+              }
+            }
+            continue;
+          } for(let k = 0; k < length; k++ ){
+                let condition = square[i][j]['indices'][0].i != k && square[i][j]['indices'][1].i != k;
+                if(square[i][j]['indices'][2] != undefined){
+                  condition = condition && (square[i][j]['indices'][2].i != k);
+                }
+               
+                if(condition){
+                   field[k][square[i][j]['indices'][0].j][i] = false;
+                  debY.push({
+                    i:k,
+                    j:square[i][j]['indices'][0].j,
+                    value:i
+                  });
+                }
+          
+        }
+      }
+      }
+      // console.dir('debX');
+      // console.dir(debX);
+      // console.dir('debY');
+      // console.dir(debY);
+      return square;
+    }
+  
+
   let field = createField(matrix);
+
+  let count = 0;
 
   do{
     lastHero(matrix,field);
+    // drawField(matrix,field,'lastHero'+count);
     checkSquare(matrix,field);
+    // drawField(matrix,field,'checkSquare'+count);
     hiddenCouples(field);
+    // drawField(matrix,field,'hiddenCouples'+count);
+  
+    // console.dir(lockedCandidate(field));
+    lockedCandidate(field)
+    // drawField(matrix,field,'lockedCandidate'+count);
+
     insertAnswerValue(matrix,field);
-  }while(checkForEnd(field) && !!debug.length);
+    // drawField(matrix,field,'insertAnswerValue '+count);
+    count++;
+  }while(checkForEnd(field) && !!debug.length && count < 30);
 
   
   return matrix;
